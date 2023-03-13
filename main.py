a6329be15955b36bcf68167aefec8ccdf0e4e389
lior motola <lior.motola@campus.technion.ac.il>
2023-03-13 16:22:18 +0200

    added documentation to main.py

--- main.py
+++ main.py
@@ -1,50 +1,57 @@
-import torch.cuda
-import pandas as pd
-from processing import create_train_df, create_translation_df_from_list , postprocess_text
-from project_evaluate import read_file
+from processing import create_translation_df , postprocess_text
 import project_evaluate
 from datasets import Dataset, DatasetDict
 from transformers import AutoModelForSeq2SeqLM, DataCollatorForSeq2Seq, Seq2SeqTrainingArguments, Seq2SeqTrainer, AutoTokenizer
 import numpy as np
 import evaluate
-from try_use_roots import create_data_with_roots, create_translation_df_with_roots
+from try_use_roots import create_data_with_roots, create_translation_df_with_roots, create_translation_df_val_with_roots,read_file_unlabeled_with_roots
 
 tokenizer = AutoTokenizer.from_pretrained("t5-base")
 
 def compute_metrics(eval_preds):
+    """
+    function to compute eval - sacrebleu metric to be used in the training procedure. Also saves the computed metrics to file.
+    :param eval_preds: tuple of preds and labels to be evaluated
+    :return: dictionary containing the bleu results and the mean len of generated sentences
+    """
 
     metric = evaluate.load("sacrebleu")
     preds, labels = eval_preds
     if isinstance(preds, tuple):
         preds = preds[0]
-    decoded_preds = tokenizer.batch_decode(preds, skip_special_tokens=True)
+
+    decoded_predictions = tokenizer.batch_decode(preds, skip_special_tokens=True)
 
     # Replace -100 in the labels as we can't decode them.
     labels = np.where(labels != -100, labels, tokenizer.pad_token_id)
     decoded_labels = tokenizer.batch_decode(labels, skip_special_tokens=True)
 
-    decoded_preds, decoded_labels = postprocess_text(decoded_preds, decoded_labels)
+    decoded_predictions, decoded_labels = postprocess_text(decoded_predictions, decoded_labels)
 
-    result = metric.compute(predictions=decoded_preds, references=decoded_labels)
+    result = metric.compute(predictions=decoded_predictions, references=decoded_labels)
     result = {"bleu": result["score"]}
 
     prediction_lens = [np.count_nonzero(pred != tokenizer.pad_token_id) for pred in preds]
     result["gen_len"] = np.mean(prediction_lens)
 
     result = {k: round(v, 4) for k, v in result.items()}
-    file = open("15e5_roots.txt", "a")
+
+    #saving results to file
+    file = open("15e5_roots2.txt", "a")
     print(result,file=file)
     print("\n",file=file)
     file.close()
+
     return result
 
 
 def preprocess_function(samples):
     """
-    This function performs preprocessing to data before sending it to the model. This preprocessing includes
-    adding a prefix of the task
-    :param samples:
-    :return:
+    This function performs preprocessing to the data before sending it to the model. This preprocessing includes
+    adding a prefix of the task "translate German to English" ,tokenizing, truncating and adding padding to the data sentences.
+    :param samples: dictionary with the key "translation" holding samples where each sample is a dictionary with the keys "de","en"
+    holding the data to be preprocessed.
+    :return: preprocessed dataset, including original data, tokenized inputs, attention masks and tokenized labels.
     """
     prefix = "translate German to English "
 
@@ -67,14 +74,14 @@ def preprocess_function(samples):
 def train(train_dataset, test_dataset, batch_size,num_epochs):
     """
     This method performs the training of the model, using hugging face trainer. It trains a t5 model on a given data.
-    The trained model is saved to the location saved in output_dir variable in the first line.
+    The trained model is saved to the location appears in output_dir variable in the first line.
     :param train_dataset: train dataset object
     :param test_dataset: test dataset
     :param batch_size: batch size
     :param num_epochs: number of epochs
 
     """
-    output_dir = "t5-base-translation-from-German-to-English-with_15e5_and_roots"
+    output_dir = "t5-base-translation-from-German-to-English-with_15e5_and_val_true_roots"
     args = Seq2SeqTrainingArguments(
         output_dir,
         evaluation_strategy = "epoch",
@@ -106,26 +113,15 @@ def train(train_dataset, test_dataset, batch_size,num_epochs):
     trainer.train()
 
 
-
 def main():
-    train_df = create_data_with_roots("data/train.labeled")
-    test_df = create_data_with_roots("data/val.labeled")
 
-    # train_df = pd.read_csv("train_data_new.csv")
-    # test_df = pd.read_csv("test_data_new.csv")
-    # train_text
-    # with open("data/train.labeled", "r", encoding='utf8') as train_file:
-    #     train_text = train_file.read()
-    # train_df = create_train_df()
-    # train_file_en, train_file_de = project_evaluate.read_file("data/train.labeled")
-    # val_en,val_de = project_evaluate.read_file("data/val.labeled")
-    #
-    # train_translation_df = create_translation_df(train_df)
-    # val_translation_df = create_translation_df(test_df)
-    train_translation_df = create_translation_df_with_roots(train_df)
-    val_translation_df = create_translation_df_with_roots(test_df)
-    # train_translation_df = create_translation_df_from_list(german = train_file_de,english=train_file_en)
-    # val_translation_df = create_translation_df_from_list(german=val_de,english=val_en)
+    #reading data files
+    train_file_en, train_file_de = project_evaluate.read_file("data/train.labeled")
+    val_en,val_de = project_evaluate.read_file("data/val.labeled")
+
+    #creating initial datasets
+    train_translation_df = create_translation_df(german=train_file_de, english=train_file_en)
+    val_translation_df = create_translation_df(german=val_de, english=val_en)
 
     train_dataset = Dataset.from_pandas(train_translation_df)
     validation_dataset = Dataset.from_pandas(val_translation_df)
@@ -134,12 +130,45 @@ def main():
     initial_datasets['train'] = train_dataset
     initial_datasets['validation'] = validation_dataset
 
+    # tokenizing the datasets
     tokenized_datasets = initial_datasets.map(preprocess_function, batched=True)
+    # training
+    train(tokenized_datasets['train'], tokenized_datasets['validation'], 5, 10)
+
+
+def main_roots():
+
+    #create initial training data
+    train_df = create_data_with_roots("data/train.labeled")
+    train_translation_df = create_translation_df_with_roots(train_df)
+
+    # create initial validation data
+    file_de, roots, modifiers = read_file_unlabeled_with_roots("data/val.unlabeled")
+    file_en, file_de_labeled = project_evaluate.read_file("data/val.labeled")
+    for sen1, sen2 in zip(file_de, file_de_labeled):
+        if sen1.strip() != sen2.strip():
+            raise ValueError('Different Sentences')
+
+    val_translation_df = create_translation_df_val_with_roots(file_de, roots, modifiers,file_en)
+
+    # creating dataset objects
+    train_dataset = Dataset.from_pandas(train_translation_df)
+    validation_dataset = Dataset.from_pandas(val_translation_df)
+
+    initial_datasets = DatasetDict()
+    initial_datasets['train'] = train_dataset
+    initial_datasets['validation'] = validation_dataset
+
+    #tokenizing the datasets
+    tokenized_datasets = initial_datasets.map(preprocess_function, batched=True)
+
+    #training
     train(tokenized_datasets['train'],tokenized_datasets['validation'],5,10)
 
 
 if __name__ == '__main__':
 
-    main()
+    # main()
+    main_roots()
 
 
