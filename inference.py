a2806db389e63e85b76a6f31b0e3a447fcaf8afc
lior motola <lior.motola@campus.technion.ac.il>
2023-03-13 16:10:47 +0200

    added documentation to inference.py

--- inference.py
+++ inference.py
@@ -10,6 +10,11 @@ tokenizer = AutoTokenizer.from_pretrained(model_checkpoint)
 
 
 def read_file_unlabeled(file_path):
+    """
+    Given a file in unlabeled format, parsing the file and returns only the german sentenes
+    :param file_path: path of file to parse
+    :return: list of sentences in german
+    """
     file_de = []
     with open(file_path, encoding='utf-8') as f:
         cur_str, cur_list = '', []
@@ -29,30 +34,50 @@ def read_file_unlabeled(file_path):
     return file_de
 
 
-
-
 def create_raw_data_unlabeled(file_de):
+    """
+    preparing data of unlabeled files to create a dataset that will be sent to the hugging face models.
+    :param file_de: list of german sentences
+    :return: pandas df with one column called translation where each row contains a dictionary with the sentence in german
+    """
     translation = []
     for input in file_de :
-        raw = {'de': input}
-        translation.append(raw)
+        trans_dict = {'de': input}
+        translation.append(trans_dict)
 
-    dataset = pd.DataFrame()
-    dataset['translation'] = translation
-    return dataset
+    df = pd.DataFrame()
+    df['translation'] = translation
+    return df
 
-def preprocess_function_unlabeled(examples):
+def preprocess_function_unlabeled(samples):
+    """
+    This function performs preprocessing to unlabeled data before sending it to the model. This preprocessing includes
+    adding a prefix of the task "translate German to English" ,tokenizing, truncating and adding padding to the data sentences.
+    :param samples: dictionary with the key "translation" holding samples where each sample is a dictionary with the key "de"
+    holding the data to be preprocessed.
+    :return: preprocessed dataset, including original data, tokenized inputs and attention masks.
+    """
     prefix = "translate German to English "
 
     max_input_length = 256
     source_lang = "de"
 
-    inputs = [prefix + ex[source_lang] for ex in examples["translation"]]
+    inputs = [prefix + ex[source_lang] for ex in samples["translation"]]
     model_inputs = tokenizer(inputs, max_length=max_input_length, truncation=True,padding=True)
     return model_inputs
 
 
 def create_translation_df_unlabeled_with_roots(file_de,roots,modifiers):
+    """
+    Gets a list of sentences in German , roots of their translation in english and list of roots' modifiers.
+    composing out of the lists new sentences where each sentence is composed of a prefix of the roots+modifiers and then the german sentence.
+    saves the new sentences to pandas df.
+    :param file_de: list of sentences in german
+    :param roots: list of words which are the roots of the english translation of the sentences in file_de
+    :param modifiers: list of tuples of modifiers corresponding to the roots in roots list.
+    :return: pandas df with one column called translation where each row contains a dictionary with the sentence in german with a prefix of the
+     English roots and modifiers.
+    """
     translation = []
 
     for input,root_str,modifiers_str in zip(file_de,roots,modifiers) :
@@ -69,14 +94,20 @@ def create_translation_df_unlabeled_with_roots(file_de,roots,modifiers):
         raw = {'de': input}
         translation.append(raw)
 
-    dataset = pd.DataFrame()
-    dataset['translation'] = translation
-    return dataset
+    df = pd.DataFrame()
+    df['translation'] = translation
+    return df
 
 
 
 
 def perform_inference(de_input_list,model_checkpoint):
+    """
+    given a pretrained model and list of sentences in german, performs inference and generates english translation of the germans inputs
+    :param de_input_list: list of sentences in german to be translated
+    :param model_checkpoint: path to the pretrained model to load
+    :return: list of the sentences from de_input_list translated to english.
+    """
 
     val_df = create_raw_data_unlabeled(de_input_list)
     val_dataset = Dataset.from_pandas(val_df)
@@ -92,6 +123,12 @@ def perform_inference(de_input_list,model_checkpoint):
     return translated_text
 
 def perform_inference_with_roots(file_path,model_checkpoint):
+    """
+    given a pretrained model and unlabeled file, performs inference and generates english translation of the germans inputs from the file
+    :param file_path: path of unlabeled file on which you want to generate translation
+    :param model_checkpoint: path to the pretrained model to load
+    :return: 2 lists one of the german inputs and one of the generated translation in english.
+    """
     file_de, roots, modifiers= read_file_unlabeled_with_roots(file_path)
     val_df = create_translation_df_unlabeled_with_roots(file_de=file_de,roots=roots,modifiers=modifiers)
     val_dataset = Dataset.from_pandas(val_df)
@@ -105,7 +142,3 @@ def perform_inference_with_roots(file_path,model_checkpoint):
     translated_text = tokenizer.batch_decode(translation, skip_special_tokens=True)
 
     return file_de, translated_text
-
-
-if __name__ == '__main__':
-    perform_inference_with_roots("data/val.unlabeled",model_checkpoint="/home/student/Final Project/Lior/t5-base-translation-from-German-to-English-with_15e5_and_roots/checkpoint-14000")
